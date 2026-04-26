32622c8b897581bd0384a23ac50ff7ed29bd594d
Gourav Yadav <you@exam>
2026-04-26 17:04:44 +0530

    chore(backend): update main application configuration

--- Capstone_VehicleRentalSystem/backend/rental/src/main/java/com/vehicle/rental/RentalApplication.java
+++ Capstone_VehicleRentalSystem/backend/rental/src/main/java/com/vehicle/rental/RentalApplication.java
@@ -2,12 +2,16 @@ package com.vehicle.rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class RentalApplication {
+@EnableScheduling
+public class
+RentalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RentalApplication.class, args);
+
 	}
 
 }
